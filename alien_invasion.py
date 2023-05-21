30ff04f67c50a9e95c843c60016bdb1e22477ca0
TuChuan <965806497@qq.com>
2023-05-21 22:38:38 +0800

    Finish Chapter 12

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,20 +1,21 @@
-import sys
 import pygame
+import game_funcations as gf
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
+    bullets = Group()
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            screen.fill(ai_settings.bg_color)
-            ship.blitme()
-            pygame.display.flip()
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,bullets)
 run_game()
